52477de7f9143315288e52b11f92bfbc17144275
Mario Galeno <mgaleno@solstice.com>
2019-11-23 01:22:07 -0600

    add react-router, active states and underline animations on hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
 import './App.css';
 import { MainHeader } from './layout/main-header';
 
 
 const App: React.FC = () => {
   return (
-    <>
+    <Router>
       <MainHeader />
-    </>
+      <main>
+        <Switch>
+          <Route path='/'>
+            <h1>WORK</h1>
+          </Route>
+
+          <Route path='/about'>
+            <h1>ABOUT</h1>
+          </Route>
+
+          <Route path='/contact'>
+            <h1>CONTACT</h1>
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
--- src/layout/main-header/navigation/index.tsx
+++ src/layout/main-header/navigation/index.tsx
@@ -1,12 +1,20 @@
 import React, { ReactElement } from 'react';
 import styles from './index.module.scss';
+import { NavLink } from 'react-router-dom';
 
 export const NavBar = (): ReactElement => {
     return (
         <nav className={styles.navWrapper}>
-            <a href='/work' className={styles.navItem}>Work</a>
-            <a href='/about' className={styles.navItem}>About</a>
-            <a href='/contact' className={styles.navItem}>Contact</a>
+            <NavLink to='/' className={styles.navItem} activeClassName={styles.activeNavItem}>
+                Work
+            </NavLink>
+            <NavLink to='/about' className={styles.navItem} activeClassName={styles.activeNavItem}>
+                About
+            </NavLink>
+            <NavLink to='/contact' className={styles.navItem} activeClassName={styles.activeNavItem}>
+                Contact
+            </NavLink>
+            <div className={styles.underline}></div>
         </nav>
     )
 }
