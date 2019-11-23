0e5642168c529424dbcbd81b7cc627409a705196
Mario Galeno <mgaleno@solstice.com>
2019-11-23 11:38:59 -0600

    Fix root active state

--- src/layout/main-header/navigation/index.tsx
+++ src/layout/main-header/navigation/index.tsx
@@ -1,20 +1,40 @@
 import React, { ReactElement } from 'react';
 import styles from './index.module.scss';
-import { NavLink } from 'react-router-dom';
+import { NavLink, match } from 'react-router-dom';
+import { Location } from 'history';
 
 export const NavBar = (): ReactElement => {
     return (
         <nav className={styles.navWrapper}>
-            <NavLink to='/' className={styles.navItem} activeClassName={styles.activeNavItem}>
+            <NavLink
+                to='/'
+                className={styles.navItem}
+                activeClassName={styles.activeNavItem}
+                isActive={isActive}
+            >
                 Work
             </NavLink>
-            <NavLink to='/about' className={styles.navItem} activeClassName={styles.activeNavItem}>
+            <NavLink
+                to='/about'
+                className={styles.navItem}
+                activeClassName={styles.activeNavItem}
+            >
                 About
             </NavLink>
-            <NavLink to='/contact' className={styles.navItem} activeClassName={styles.activeNavItem}>
+            <NavLink
+                to='/contact'
+                className={styles.navItem}
+                activeClassName={styles.activeNavItem}
+            >
                 Contact
             </NavLink>
             <div className={styles.underline}></div>
         </nav>
     )
+}
+
+const isActive = (match: match, location: Location<any>): boolean => {
+    if (!location) return false;
+    const { pathname } = location;
+    return pathname === '/';
 }
