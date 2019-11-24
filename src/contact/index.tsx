b3ded78529ae3d4de462cb6edab3dbc282eeeea0
Mario Galeno <mgaleno@solstice.com>
2019-11-24 00:15:26 -0600

    Add input form for contact

--- src/contact/index.tsx
+++ src/contact/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Main } from '../layout/content/main';
+import styles from './index.module.scss';
 
 export const Contact = () => {
     return (
         <Main>
-            <h1>Contact</h1>
+            <>
+                <div className={styles.contactDetails}>
+                    <p>Aurora 'Ro' Grajeda</p>
+                    <p>ro@auroragrajeda.com</p>
+                    <p>Chicago, IL</p>
+                </div>
+                {/* 
+                // @ts-ignore */}
+                <form name="contactForm" netlify netlify-honeypot="bot-field" className={styles.form}>
+                    <div className={styles.fields}>
+                        <section className={styles.formSection}>
+                            <input type='text' name='name' placeholder='Name' required />
+                            <input type='email' name='email' placeholder='Email' required />
+                            <input type='text' name='subject' placeholder='Subject' required />
+                        </section>
+                        <section className={styles.formSection}>
+                            <textarea name='message' placeholder='Message' required></textarea>
+                        </section>
+                    </div>
+                    <p style={{ display: 'none' }}>
+                        <label>Don’t fill this out: <input name="bot-field" /></label>
+                    </p>
+                    <button type="submit" className={styles.button}>Send</button>
+                </form>
+            </>
         </Main>
     )
 }
