99f2a32ef2503863012b3f9f1ee39e3b243cc754
Mario Galeno <galeno.mario@gmail.com>
2019-12-05 22:35:50 -0600

    Convert <a> to <NavLink> for title image

--- src/layout/main-header/logo/index.tsx
+++ src/layout/main-header/logo/index.tsx
@@ -1,11 +1,12 @@
 import React, { ReactElement } from 'react';
 import logo from '../../../assets/squeegee-logo.png'
 import styles from './index.module.scss';
+import { NavLink } from 'react-router-dom';
 export const Logo = (): ReactElement => {
   return (
-    <a href='/' className={styles.logoWrapper}>
+    <NavLink to='/' className={styles.logoWrapper}>
       <img src={logo} alt='' className={styles.logo} />
       <h1 className={styles.title}>Aurora Grajeda</h1>
-    </a>
+    </NavLink>
   )
 };
